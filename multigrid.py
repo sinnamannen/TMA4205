c66fc940585943a8f64226679a09f75e8e165389
Viktor Sandve <viktorsandve@gmail.com>
2025-11-02 18:47:28 +0100

    Function to load and preprocess images, and start implementing multigrid

---
+++ multigrid.py
@@ -0,0 +1,189 @@
+import numpy as np
+
+def laplacian(u):
+    n, m = u.shape
+    laplace_u = np.zeros_like(u)
+    laplace_u[1:n-1,1:m-1] = -4*u[1:n-1,1:m-1] + u[0:n-2,1:m-1] + u[2:n,1:m-1] + u[1:n-1,0:m-2] + u[1:n-1,2:m]
+
+    return laplace_u
+
+def OF_cg(u0,v0,Ix,Iy,reg,rhsu,rhsv,tol=1.e-8,maxit=2000):
+    '''
+    The CG method for the optimal flow problem
+    input:
+    u0 - initial guess for u
+    v0 - initial guess for v
+    Ix - x-derivative of the first frame
+    Iy - y-derivative of the first framereg - regularisation parameter lmbda
+    rhsu - right-hand side in the equation for u
+    rhsv - right-hand side in the equation for v
+    tol - relative residual tolerance
+    maxit - maximum number of iterations
+    output:
+    u - numerical solution for u
+    v - numerical solution for v
+    '''
+    
+    return 0
+
+
+def V_cycle(u0, v0, Ix, Iy, lmbda, reg, rhsu, rhsv, s1, s2, level, max_level):
+    '''
+    V-cycle for the optical flow problem.
+    input:
+    u0 - initial guess for u
+    v0 - initial guess for v
+    Ix - x-derivative of the first frame
+    Iy - y-derivative of the first frame
+    reg - regularisation parameter (lmbda)
+    rhsu - right-hand side in the equation for u
+    rhsv - right-hand side in the equation for v
+    s1 - number of pre-smoothings
+    s2 - number of post-smoothings
+    level - current level
+    max_level - total number of levels
+    output:
+    u - numerical solution for u
+    v - numerical solution for v
+    '''
+    
+    u,v = smoothing(u0, v0, Ix, Iy, reg, rhsu, rhsv, level,s1)
+    rhu,rhv = residual(u, v, Ix, Iy, reg, rhsu, rhsv)
+    r2hu,r2hv,Ix2h,Iy2h = restriction(rhu, rhv, Ix, Iy)
+    
+    if level == max_level - 1:
+        e2hu,e2hv = OF_cg(np.zeros_like(r2hu), np.zeros_like(r2hv),
+            Ix2h, Iy2h, reg, rhsu, rhsv, 1e-8, 1000, level+1)
+    else:
+        e2hu,e2hv = V_cycle(np.zeros_like(r2hu), np.zeros_like(size(r2hv)),
+            Ix2h, Iy2h, reg, r2hu, r2hv, s1, s2, level+1, max_level)
+        
+    ehu,ehv = prolongation(e2hu, e2hv)
+    u = u + ehu
+    v = v + ehv
+    u,v = smoothing(u, v, Ix, Iy, lmbda, rhsu, rhsv, level, s2)
+    return u, v
+
+def smoothing(u0, v0, Ix, Iy, reg, rhsu, rhsv, level, s):
+    '''
+    Smoothing for the optical flow problem.
+    Using Red-Black Gauss-Seidel smoothing.
+    input:
+    u0 - initial guess for u
+    v0 - initial guess for v
+    Ix - x-derivative of the first frame
+    Iy - y-derivative of the first frame
+    reg - regularisation parameter (lmbda)
+    rhsu - right-hand side in the equation for u
+    rhsv - right-hand side in the equation for v
+    level - current level
+    s - number of smoothing iterations
+    output:
+    u - numerical solution for u
+    v - numerical solution for v
+    '''
+    n,m = u0.shape
+    u = u0.copy()
+    v = v0.copy()
+    for _ in range(s):
+        u, v = RBGS_step(u, v, Ix, Iy, reg, rhsu, rhsv, n, m)
+    return u, v
+
+def RBGS_step(u, v, Ix, Iy, reg, rhsu, rhsv, n, m):
+    #TODO: Think about boundary....
+    for i in range(1,n-1):
+            for j in range(1,m-1):
+                if (i+j) % 2 == 0:
+                    u[i,j] = 0.25 * (u[i-1,j] + u[i+1,j] + u[i,j-1] + u[i,j+1] + rhsu[i,j])
+                    v[i,j] = 0.25 * (v[i-1,j] + v[i+1,j] + v[i,j-1] + v[i,j+1] + rhsv[i,j])
+    for i in range(1,n-1):
+            for j in range(1,m-1):
+                if (i+j) % 2 == 1:
+                    u[i,j] = 0.25 * (u[i-1,j] + u[i+1,j] + u[i,j-1] + u[i,j+1] + rhsu[i,j])
+                    v[i,j] = 0.25 * (v[i-1,j] + v[i+1,j] + v[i,j-1] + v[i,j+1] + rhsv[i,j])
+    return u, v
+
+def residual(u, v, Ix, Iy, reg, rhsu, rhsv):
+    '''
+    Compute the residual for the optical flow problem.
+    input:
+    u - current solution for u
+    v - current solution for v
+    Ix - x-derivative of the first frame
+    Iy - y-derivative of the first frame
+    reg - regularisation parameter (lmbda)
+    rhsu - right-hand side in the equation for u
+    rhsv - right-hand side in the equation for v
+    output:
+    rhu - residual for u
+    rhv - residual for v
+    '''
+    
+    rhu = rhsu - (Ix * (Ix * u + Iy * v) + reg * laplacian(u))
+    rhv = rhsv - (Iy * (Ix * u + Iy * v) + reg * laplacian(v))
+    return rhu, rhv
+
+def restriction(rhu, rhv, Ix, Iy):
+    '''
+    Restriction operator for the optical flow problem.
+    input:
+    rhu - residual for u
+    rhv - residual for v
+    Ix - x-derivative of the first frame
+    Iy - y-derivative of the first frame
+    output:
+    r2hu - restricted residual for u
+    r2hv - restricted residual for v
+    Ix2h - restricted x-derivative of the first frame
+    Iy2h - restricted y-derivative of the first frame
+    '''
+
+    r2hu = 0.25 * (rhu[0::2, 0::2] + rhu[1::2, 0::2] + rhu[0::2, 1::2] + rhu[1::2, 1::2])
+    r2hv = 0.25 * (rhv[0::2, 0::2] + rhv[1::2, 0::2] + rhv[0::2, 1::2] + rhv[1::2, 1::2])
+    Ix2h = 0.25 * (Ix[0::2, 0::2] + Ix[1::2, 0::2] + Ix[0::2, 1::2] + Ix[1::2, 1::2])
+    Iy2h = 0.25 * (Iy[0::2, 0::2] + Iy[1::2, 0::2] + Iy[0::2, 1::2] + Iy[1::2, 1::2])
+    
+    return r2hu, r2hv, Ix2h, Iy2h
+
+def prolongation(e2hu, e2hv):
+    '''
+    Prolongation operator for the optical flow problem.
+    input:
+    e2hu - error on the coarse grid for u
+    e2hv - error on the coarse grid for v
+    output:
+    ehu - prolonged error for u
+    ehv - prolonged error for v
+    '''
+    n,m = e2hu.shape
+    ehu = np.zeros((2*n, 2*m))
+    ehv = np.zeros((2*n, 2*m))
+    
+    #internal points
+    ehu[1:-1,1:-1] = 0.25 * (e2hu[:-1,:-1] + e2hu[1:,:-1] + e2hu[:-1,1:] + e2hu[1:,1:])
+    ehv[1:-1,1:-1] = 0.25 * (e2hv[:-1,:-1] + e2hv[1:,:-1] + e2hv[:-1,1:] + e2hv[1:,1:])
+    
+    #upper boundary
+    ehu[0,1:-1] = 0.5 * (e2hu[0,:-1] + e2hu[0,1:])
+    ehv[0,1:-1] = 0.5 * (e2hv[0,:-1] + e2hv[0,1:])
+    #lower boundary
+    ehu[-1,1:-1] = 0.5 * (e2hu[-1,:-1] + e2hu[-1,1:])
+    ehv[-1,1:-1] = 0.5 * (e2hv[-1,:-1] + e2hv[-1,1:])
+    #left boundary
+    ehu[1:-1,0] = 0.5 * (e2hu[:-1,0] + e2hu[1:,0])
+    ehv[1:-1,0] = 0.5 * (e2hv[:-1,0] + e2hv[1:,0])
+    #right boundary
+    ehu[1:-1,-1] = 0.5 * (e2hu[:-1,-1] + e2hu[1:,-1])
+    ehv[1:-1,-1] = 0.5 * (e2hv[:-1,-1] + e2hv[1:,-1])
+    
+    #corners
+    ehu[0,0] = e2hu[0,0]
+    ehv[0,0] = e2hv[0,0]
+    ehu[0,-1] = e2hu[0,-1]
+    ehv[0,-1] = e2hv[0,-1]
+    ehu[-1,0] = e2hu[-1,0]
+    ehv[-1,0] = e2hv[-1,0]
+    ehu[-1,-1] = e2hu[-1,-1]
+    ehv[-1,-1] = e2hv[-1,-1]
+    
+    return ehu, ehv
