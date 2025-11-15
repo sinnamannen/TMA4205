79c553aa5257d03a515d3aee6aad2422fc9fbceb
Viktor Sandve <viktorsandve@gmail.com>
2025-11-15 11:23:27 +0100

    Fix multigrid and pcg

--- multigrid.py
+++ multigrid.py
@@ -12,48 +12,7 @@ def multigrid_main(img1, img2, reg, s1, s2, max_level, from_file=False, sigma=0)
     u, v = V_cycle(u0, v0, Ix, Iy, reg, rhsu, rhsv, s1, s2, 0, max_level)
     return u, v
 
-    
-
-
-def V_cycle(u0, v0, Ix, Iy, reg, rhsu, rhsv, s1, s2, level, max_level):
-    '''
-    V-cycle for the optical flow problem.
-    input:
-    u0 - initial guess for u
-    v0 - initial guess for v
-    Ix - x-derivative of the first frame
-    Iy - y-derivative of the first frame
-    reg - regularisation parameter (lmbda)
-    rhsu - right-hand side in the equation for u
-    rhsv - right-hand side in the equation for v
-    s1 - number of pre-smoothings
-    s2 - number of post-smoothings
-    level - current level
-    max_level - total number of levels
-    output:
-    u - numerical solution for u
-    v - numerical solution for v
-    '''
-    
-    u,v = smoothing(u0, v0, Ix, Iy, reg, rhsu, rhsv, level,s1)
-    rhu,rhv = residual(u, v, Ix, Iy, reg, rhsu, rhsv)
-    r2hu,r2hv,Ix2h,Iy2h = restriction(rhu, rhv, Ix, Iy)
-    if level == max_level - 1:
-        cg_reg = reg * 4**(-level)
-        
-        e2hu,e2hv, _, _, _ = OF_cg(np.zeros_like(r2hu), np.zeros_like(r2hv),
-            Ix2h, Iy2h, cg_reg, r2hu, r2hv, 1e-8, 1000)
-    else:
-        e2hu,e2hv = V_cycle(np.zeros_like(r2hu), np.zeros_like(r2hv),
-            Ix2h, Iy2h, reg, r2hu, r2hv, s1, s2, level+1, max_level)
-    ehu,ehv = prolongation(e2hu, e2hv)
-    u = u + ehu
-    v = v + ehv
-    u,v = smoothing(u, v, Ix, Iy, reg, rhsu, rhsv, level, s2)
-    return u, v
-
 def smoothing(u0, v0, Ix, Iy, reg, rhsu, rhsv, level, s):
-    
     '''
     Smoothing for the optical flow problem.
     Using Red-Black Gauss-Seidel smoothing.
@@ -71,7 +30,7 @@ def smoothing(u0, v0, Ix, Iy, reg, rhsu, rhsv, level, s):
     u - numerical solution for u
     v - numerical solution for v
     '''
-   
+    
     reg = reg * 4**(-level)
     denom_u = Ix**2 + reg*4
     denom_v = Iy**2 + reg*4
@@ -83,6 +42,7 @@ def smoothing(u0, v0, Ix, Iy, reg, rhsu, rhsv, level, s):
     for _ in range(s):
         u, v = RBGS_step(u, v, denom_u, denom_v, Ixy, reg, rhsu, rhsv)
     return u[1:-1,1:-1], v[1:-1,1:-1]
+    
 
 def RBGS_step(u, v, denom_u, denom_v, Ixy, reg, rhsu, rhsv):
     '''
@@ -132,8 +92,6 @@ def RBGS_step(u, v, denom_u, denom_v, Ixy, reg, rhsu, rhsv):
     return u, v
     
 
-
-
 def restriction(rhu, rhv, Ix, Iy):
     '''
     Restriction operator for the optical flow problem.
@@ -156,6 +114,8 @@ def restriction(rhu, rhv, Ix, Iy):
     
     return r2hu, r2hv, Ix2h, Iy2h
 
+
+
 def prolongation(e2hu, e2hv):
     '''
     Prolongation operator for the optical flow problem.
@@ -166,13 +126,59 @@ def prolongation(e2hu, e2hv):
     ehu - prolonged error for u
     ehv - prolonged error for v
     '''
-    n,m = e2hu.shape
-    ehu = np.zeros((2*n, 2*m))
-    ehv = np.zeros((2*n, 2*m))
+    m,n = e2hu.shape
+    ehu = np.zeros((2*m, 2*n))
+    ehv = np.zeros((2*m, 2*n))
     
     ehu[0::2, 0::2] = e2hu
     ehu[1::2, 0::2] = e2hu
     ehu[0::2, 1::2] = e2hu
     ehu[1::2, 1::2] = e2hu
     
+    ehv[0::2, 0::2] = e2hv
+    ehv[1::2, 0::2] = e2hv
+    ehv[0::2, 1::2] = e2hv
+    ehv[1::2, 1::2] = e2hv
+    
     return ehu, ehv
+
+
+
+def V_cycle(u0, v0, Ix, Iy, reg, rhsu, rhsv, s1, s2, level, max_level):
+    '''
+    V-cycle for the optical flow problem.
+    input:
+    u0 - initial guess for u
+    v0 - initial guess for v
+    Ix - x-derivative of the first frame
+    Iy - y-derivative of the first frame
+    reg - regularisation parameter (lmbda)
+    rhsu - right-hand side in the equation for u
+    rhsv - right-hand side in the equation for v
+    s1 - number of pre-smoothings
+    s2 - number of post-smoothings
+    level - current level
+    max_level - total number of levels
+    output:
+    u - numerical solution for u
+    v - numerical solution for v
+    '''
+    
+    u,v = smoothing(u0, v0, Ix, Iy, reg, rhsu, rhsv, level,s1)
+    rhu,rhv = residual(u, v, Ix, Iy, reg * 4**(-level), rhsu, rhsv)
+    r2hu,r2hv,Ix2h,Iy2h = restriction(rhu, rhv, Ix, Iy)
+    if level == max_level - 1:
+        cg_reg = reg * 4**(-max_level)
+        
+        e2hu,e2hv, _, _, _ = OF_cg(np.zeros_like(r2hu), np.zeros_like(r2hv),
+            Ix2h, Iy2h, cg_reg, r2hu, r2hv, 1e-8, 1000)
+    else:
+        e2hu,e2hv = V_cycle(np.zeros_like(r2hu), np.zeros_like(r2hv),
+            Ix2h, Iy2h, reg, r2hu, r2hv, s1, s2, level+1, max_level)
+    ehu,ehv = prolongation(e2hu, e2hv)
+    u = u + ehu
+    v = v + ehv
+    u,v = smoothing(u, v, Ix, Iy, reg, rhsu, rhsv, level, s2)
+    return u, v
+  
+
